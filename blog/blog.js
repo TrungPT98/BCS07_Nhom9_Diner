f4aeb4771238187244e4c59c007f3a70fefeb02d
RayOlardo <phuclam1044@gmail.com>
2023-05-03 08:46:30 +0700

    push code lan n

---
+++ blog/blog.js
@@ -0,0 +1,6 @@
+var openOverlay = document.getElementById('open__Overlay').onclick = function() {
+    var overlay = document.getElementById('overLay').classList.add('overlay__open') 
+}
+var closeOverlay = document.getElementById('closeOverlay').onclick = function() {
+    var overLay = document.getElementById('overLay').classList.remove('overlay__open')
+}
